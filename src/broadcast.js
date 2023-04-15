0ae0f29b9be3bb314ba91af1ebc1010ed1291a3a
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-04-15 11:54:41 +0200

    Create async to send new score to api

---
+++ src/broadcast.js
@@ -0,0 +1,16 @@
+// send new score to API
+
+export default async function broadcast(raw) {
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+  const requestURL =
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/dV4aWISG7S2Gi5e0alOq/scores';
+  const request = new Request(requestURL);
+  await fetch(request, requestOptions);
+}
